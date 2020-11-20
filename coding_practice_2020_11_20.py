de30e47ba5b1ee85a5e80a4cef9b689975f3e601
zhu-sunny <74637858+zhu-sunny@users.noreply.github.com>
2020-11-20 13:26:28 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -3,3 +3,21 @@ print("What is your favorite color?")
 color = input()
 print("Cool!" + color + " is my favorite color too!")
 
+2.
+cans = int(input("How many cans come in a pack?"))
+packs = int(input("How many packs are there?"))
+print("There are " + str(cans*packs) + " cans in total.")
+
+3.
+length = int(input("Please enter the length"))
+width = int(input("Please enter the width"))
+height = int(input("Please enter the height"))
+print("The volume of the rectangular prism is " + str(length*width*height) + "cm cubed.")
+
+4. 
+answer = input("Do you just join a google meet and mute the teacher?")
+if(answer == "yes"):
+    print("That's probably not a good idea")
+elif(answer == "no"):
+    print("Ok. Good.")
+
